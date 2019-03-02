20dc81f015df8bc3472ec5c4e6c25d4601b1d355
Josh Stewart <hi@joshstewart.me>
2019-03-02 10:55:59 -0500

    Only reply if we haven't seen the message before

--- src/skills/bukkit/listener.js
+++ src/skills/bukkit/listener.js
@@ -8,22 +8,14 @@ module.exports = controller => {
     async (bot, message) => {
       try {
         if (message["event_id"] === lastEventId) {
-          console.log(
-            `*** already replied to this one. event_id: ${message["event_id"]}`
-          );
-        } else {
-          lastEventId = message["event_id"];
-          console.log(
-            `*** reply to this one once. event_id: ${message["event_id"]}`
-          );
+          return;
         }
-        bot.replyAcknowledge(() => {
-          console.log(`*** replyAcknowledge callback called`);
-        });
-        console.log(`*** message: ${JSON.stringify(message)}`);
+
+        lastEventId = message["event_id"];
+        bot.replyAcknowledge();
+
         const [, query, source] = message.match;
         const reply = await bukkitService.find(controller, query, source);
-        console.log(`*** reply: ${reply}`);
         bot.reply(message, reply);
       } catch (err) {
         bot.reply(message, `Error getting bukkit: ${err}`);
