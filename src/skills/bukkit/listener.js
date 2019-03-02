0f209f4f6a5639ea2a5cb9c9aa81ae3f4125fb45
Josh Stewart <hi@joshstewart.me>
2019-03-01 20:13:14 -0500

    Add logging to debug multiple bukkits

--- src/skills/bukkit/listener.js
+++ src/skills/bukkit/listener.js
@@ -5,10 +5,11 @@ module.exports = controller => {
     [/^bukkit\s?([\w-]+)?(?: from (\w+))?$/i],
     "direct_message, direct_mention, ambient",
     async (bot, message) => {
+      console.log(`*** message: ${JSON.stringify(message)}`);
       try {
         const [, query, source] = message.match;
         const reply = await bukkitService.find(controller, query, source);
-
+        console.log(`*** reply: ${reply}`);
         bot.reply(message, reply);
       } catch (err) {
         bot.reply(message, `Error getting bukkit: ${err}`);
