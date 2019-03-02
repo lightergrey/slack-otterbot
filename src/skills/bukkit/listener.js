0d55ea5a78d35b6518567cde548bbdf39aa9b139
Josh Stewart <hi@joshstewart.me>
2019-03-02 05:49:51 -0500

    Add logging to acknowledge callback to debug

--- src/skills/bukkit/listener.js
+++ src/skills/bukkit/listener.js
@@ -5,9 +5,11 @@ module.exports = controller => {
     [/^bukkit\s?([\w-]+)?(?: from (\w+))?$/i],
     "direct_message, direct_mention, ambient",
     async (bot, message) => {
-      bot.replyAcknowledge();
-      console.log(`*** message: ${JSON.stringify(message)}`);
       try {
+        bot.replyAcknowledge(() => {
+          console.log(`*** replyAcknowledge callback called`);
+        });
+        console.log(`*** message: ${JSON.stringify(message)}`);
         const [, query, source] = message.match;
         const reply = await bukkitService.find(controller, query, source);
         console.log(`*** reply: ${reply}`);
