526697a22f3507f308f3a6c725d654ff0d211605
Josh Stewart <hi@joshstewart.me>
2019-03-03 15:12:29 -0500

    Add test for repeat messages and make bukkit listener a unit test

--- src/skills/bukkit/listener.js
+++ src/skills/bukkit/listener.js
@@ -7,6 +7,9 @@ module.exports = controller => {
     "direct_message, direct_mention, ambient",
     async (bot, message) => {
       try {
+        /**
+          The bot was replying with multiple bukkits when a long time had ellapsed between interacting with it. My suspicion is that this was due to the heroku dyno going to sleep and the bot not acknowledging receiving the message quick enough (300ms) causing Slack to resend the message 3 times. This fix may not be necessary since we upgraded to the hobby dyno.
+        */
         if (message["event_id"] === lastEventId) {
           return;
         }
@@ -18,7 +21,9 @@ module.exports = controller => {
         const reply = await bukkitService.find(controller, query, source);
         bot.reply(message, reply);
       } catch (err) {
-        bot.reply(message, `Error getting bukkit: ${err}`);
+        const errorResponse = `'bukkit' error: ${err}`;
+        bot.reply(message, errorResponse);
+        console.error(errorResponse);
       }
     }
   );
--- src/skills/bukkit/listener.test.js
+++ src/skills/bukkit/listener.test.js
@@ -1,13 +1,9 @@
 const listener = require("./listener");
 
-global.Math = require("../../testing/mock-math-random");
 const Botmock = require("botkit-mock");
-const demoData = require("../../testing/fixtures/storage-data-bukkits-multiple-sources");
-const getMockUserInput = require("../../testing/get-mock-user-input");
-
-const storeData = data => {
-  this.controller.storage.teams.save(data, () => {});
-};
+global.console = { error: jest.fn() };
+jest.mock("../../services/bukkit");
+const bukkitService = require("../../services/bukkit");
 
 beforeEach(() => {
   this.controller = Botmock({
@@ -21,54 +17,66 @@ beforeEach(() => {
 });
 
 test("does not respond to other", () => {
-  storeData(demoData);
   const input = getMockUserInput("other", "other");
+
+  expect.assertions(2);
   return this.bot.usersInput([input]).then(message => {
     expect(this.bot.replyAcknowledge).not.toHaveBeenCalled();
     return expect(message).toMatchObject({});
   });
 });
 
-test("gives a helpful message if no bukkits found", () => {
-  const input = getMockUserInput("bukkit", "no_bukkits");
-  return this.bot.usersInput([input]).then(message => {
+test("responds with text from bukkit service find resolve", async () => {
+  const input = getMockUserInput("bukkit", "resolve");
+
+  bukkitService.find.mockResolvedValueOnce("find response text");
+
+  expect.assertions(2);
+  await this.bot.usersInput([input]).then(message => {
     expect(this.bot.replyAcknowledge).toHaveBeenCalled();
-    return expect(message.text).toEqual("No bukkits. Try `/reload-bukkits`");
+    return expect(message.text).toEqual("find response text");
   });
 });
 
-test("responds to 'bukkit' and no query", () => {
-  storeData(demoData);
-  const input = getMockUserInput("bukkit", "no_query");
-  return this.bot.usersInput([input]).then(message => {
+test("responds with text from bukkit service find reject", async () => {
+  const input = getMockUserInput("bukkit", "reject");
+  const expectedErrorMessage = "'bukkit' error: find rejected text";
+
+  bukkitService.find.mockRejectedValueOnce("find rejected text");
+
+  expect.assertions(3);
+  await this.bot.usersInput([input]).then(message => {
     expect(this.bot.replyAcknowledge).toHaveBeenCalled();
-    expect(message.text).toEqual("https://bukk.it/three.png");
+    expect(console.error).toBeCalledWith(expectedErrorMessage);
+    return expect(message.text).toEqual(expectedErrorMessage);
   });
 });
 
-test("responds to 'bukkit' and bad query", () => {
-  storeData(demoData);
-  const input = getMockUserInput("bukkit blerg", "bad_query");
-  return this.bot.usersInput([input]).then(message => {
-    expect(this.bot.replyAcknowledge).toHaveBeenCalled();
-    return expect(message.text).toEqual("Couldn’t find a match.");
+test("responds once to message with the same id", async () => {
+  const input = getMockUserInput("bukkit", "multiple");
+
+  bukkitService.find.mockResolvedValueOnce("find response text");
+
+  expect.assertions(3);
+  return this.bot.usersInput([input]).then(async message => {
+    return this.bot.usersInput([input]).then(message => {
+      expect(this.bot.replyAcknowledge).toHaveBeenCalledTimes(1);
+      expect(this.bot.detailed_answers["someChannel"]).toHaveLength(1);
+      return expect(message.text).toEqual("find response text");
+    });
   });
 });
 
-test("responds to 'bukkit' and query", () => {
-  storeData(demoData);
-  const input = getMockUserInput("bukkit two", "with_query");
-  return this.bot.usersInput([input]).then(message => {
-    expect(this.bot.replyAcknowledge).toHaveBeenCalled();
-    return expect(message.text).toEqual("https://bukk.it/two.jpg");
-  });
-});
-
-test("responds to 'bukkit' and query with source", () => {
-  storeData(demoData);
-  const input = getMockUserInput("bukkit two from flo", "with_source");
-  return this.bot.usersInput([input]).then(message => {
-    expect(this.bot.replyAcknowledge).toHaveBeenCalled();
-    return expect(message.text).toEqual("https://floops.io/two.jpg");
-  });
-});
+const getMockUserInput = (text, event_id) => {
+  return {
+    user: "someUserId",
+    channel: "someChannel",
+    messages: [
+      {
+        text,
+        isAssertion: true,
+        event_id
+      }
+    ]
+  };
+};
