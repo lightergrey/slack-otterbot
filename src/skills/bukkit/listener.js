f58a1c5abeb7416a7d44f65560eecdee7a74fb10
Josh Stewart <hi@joshstewart.me>
2019-03-03 19:44:57 -0500

    Move stuff out of try catch

--- src/skills/bukkit/listener.js
+++ src/skills/bukkit/listener.js
@@ -6,18 +6,18 @@ module.exports = controller => {
     [/^bukkit\s?([\w-]+)?(?: from (\w+))?$/i],
     "direct_message, direct_mention, ambient",
     async (bot, message) => {
+      /**
+        The bot was replying with multiple bukkits when a long time had ellapsed between interacting with it. My suspicion is that this was due to the heroku dyno going to sleep and the bot not acknowledging receiving the message quick enough (300ms) causing Slack to resend the message 3 times. This fix may not be necessary since we upgraded to the hobby dyno.
+      */
+      if (message["event_id"] === lastEventId) {
+        return;
+      }
+
+      lastEventId = message["event_id"];
+      bot.replyAcknowledge();
+      const [, query, source] = message.match;
+
       try {
-        /**
-          The bot was replying with multiple bukkits when a long time had ellapsed between interacting with it. My suspicion is that this was due to the heroku dyno going to sleep and the bot not acknowledging receiving the message quick enough (300ms) causing Slack to resend the message 3 times. This fix may not be necessary since we upgraded to the hobby dyno.
-        */
-        if (message["event_id"] === lastEventId) {
-          return;
-        }
-
-        lastEventId = message["event_id"];
-        bot.replyAcknowledge();
-
-        const [, query, source] = message.match;
         const reply = await bukkitService.find(controller, query, source);
         bot.reply(message, reply);
       } catch (err) {
--- src/skills/bukkit/slash-command.js
+++ src/skills/bukkit/slash-command.js
@@ -2,18 +2,15 @@ const bukkitService = require("../../services/bukkit");
 
 module.exports = controller => {
   controller.on("slash_command", async (bot, message) => {
+    if (message.command !== "/bukkit") {
+      return;
+    }
+
+    bot.replyAcknowledge();
+    const [, query, source] = message.text.match(/^([\w-]+)?(?: from (\w+))?$/);
+
     try {
-      if (message.command !== "/bukkit") {
-        return;
-      }
-
-      bot.replyAcknowledge();
-
-      const [, query, source] = message.text.match(
-        /^([\w-]+)?(?: from (\w+))?$/
-      );
       const reply = await bukkitService.find(controller, query, source);
-
       bot.reply(message, reply);
     } catch (err) {
       const errorResponse = `'/bukkit' error: ${err}`;
