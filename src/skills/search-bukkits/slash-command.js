995401ee7df7fc636ada1f540761dda08e8f7a63
Josh Stewart <hi@joshstewart.me>
2019-04-08 11:03:48 -0400

    Better logging

--- src/skills/search-bukkits/slash-command.js
+++ src/skills/search-bukkits/slash-command.js
@@ -23,17 +23,23 @@ module.exports = controller => {
   });
 
   controller.on("interactive_message_callback", (bot, message) => {
-    console.log(JSON.stringify(message, null, 2));
+    console.log("========= INTERACTIVE MESSAGE CALLBACK");
+    console.log(JSON.stringify(message));
+    console.log("///////// INTERACTIVE MESSAGE CALLBACK");
     bot.replyPrivate(message, "interactive_message_callback");
   });
 
   controller.on("block_actions", (bot, message) => {
-    console.log(JSON.stringify(message, null, 2));
+    console.log("========= BLOCK ACTIONS");
+    console.log(JSON.stringify(message));
+    console.log("///////// BLOCK ACTIONS");
     bot.replyPrivate(message, "block_actions");
   });
 
   controller.hears("interactive", "direct_message", (bot, message) => {
-    console.log(JSON.stringify(message, null, 2));
+    console.log("========= INTERACTIVE");
+    console.log(JSON.stringify(message));
+    console.log("///////// INTERACTIVE");
     bot.replyPrivate(message, "interactive");
   });
 };
