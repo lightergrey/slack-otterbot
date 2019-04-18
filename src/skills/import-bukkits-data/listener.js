01c433fc2b468c5d1458fe83566850954f30365f
Josh Stewart <hi@joshstewart.me>
2019-04-18 17:01:44 -0400

    Add request to read the uploaded file

--- src/skills/import-bukkits-data/listener.js
+++ src/skills/import-bukkits-data/listener.js
@@ -1,3 +1,5 @@
+const request = require("request");
+
 module.exports = controller => {
   controller.on("file_share", (bot, message) => {
     if (
@@ -5,7 +7,19 @@ module.exports = controller => {
       message.files[0] &&
       message.files[0].name === "bukkits-data.json"
     ) {
-      bot.reply(message, "this is a valid data file");
+      const url = message.files[0].url_private;
+      bot.reply(message, `this is a valid data file: ${url}`);
+
+      request.get(url, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const data = JSON.parse(body);
+          bot.replyPublicDelayed(message, "I got data!");
+          if (data.sources["https://bukk.it/"]["qaz.gif"] !== undefined) {
+            bot.replyPublicDelayed(message, "I got NEW data!");
+          }
+          console.log(data);
+        }
+      });
     }
   });
 };
