0a31c313f0e813e8cc6f0a938dfd1514540990aa
Josh Stewart <hi@joshstewart.me>
2019-04-18 17:15:36 -0400

    Use node fetch for async

--- src/skills/import-bukkits-data/listener.js
+++ src/skills/import-bukkits-data/listener.js
@@ -1,25 +1,24 @@
-const request = require("request");
+const fetch = require("node-fetch");
 
 module.exports = controller => {
-  controller.on("file_share", (bot, message) => {
+  controller.on("file_share", async (bot, message) => {
     if (
       message.files &&
       message.files[0] &&
       message.files[0].name === "bukkits-data.json"
     ) {
-      const url = message.files[0].url_private;
-      bot.reply(message, `this is a valid data file: ${url}`);
+      try {
+        const url = message.files[0].url_private;
+        bot.reply(message, `this is a valid data file: ${url}`);
 
-      request.get(url, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const data = JSON.parse(body);
-          bot.replyPublicDelayed(message, "I got data!");
-          if (data.sources["https://bukk.it/"]["qaz.gif"] !== undefined) {
-            bot.replyPublicDelayed(message, "I got NEW data!");
-          }
-          console.log(data);
-        }
-      });
+        const response = await fetch(url);
+        bot.replyPublicDelayed(message, "GOT RESPONSE");
+        console.log(response);
+
+        bot.replyPublicDelayed(message, `${response}`);
+      } catch (err) {
+        bot.replyPublicDelayed(message, `${err}`);
+      }
     }
   });
 };
