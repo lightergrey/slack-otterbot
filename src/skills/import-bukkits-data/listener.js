b5006da621ef94fff49027808f5f557e93721472
Josh Stewart <hi@joshstewart.me>
2019-04-18 16:02:16 -0400

    LOG

--- src/skills/import-bukkits-data/listener.js
+++ src/skills/import-bukkits-data/listener.js
@@ -1,5 +1,8 @@
 module.exports = controller => {
   controller.on("file_share", (bot, message) => {
+    console.log("******************");
+    console.log(JSON.stringify(message));
+    console.log("******************");
     bot.replyAcknowledge();
     bot.replyPublicDelayed(
       `\`\`\`\n${JSON.stringify(message, null, 2)}\n\`\`\``
