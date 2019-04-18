a5d039eca60d56c126a2417b06e7ccaf46eb8399
Josh Stewart <hi@joshstewart.me>
2019-04-18 15:49:37 -0400

    import test is not async

--- src/skills/import-bukkits-data/listener.js
+++ src/skills/import-bukkits-data/listener.js
@@ -1,5 +1,5 @@
 module.exports = controller => {
-  controller.on("file_share", async (bot, message) => {
+  controller.on("file_share", (bot, message) => {
     bot.reply(`\`\`\`\n${JSON.stringify(message, null, 2)}\n\`\`\``);
   });
 };
