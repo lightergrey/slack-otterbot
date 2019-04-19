ef6b93625d8cd7d4f9b425525c75c1983c6d1d07
Josh Stewart <hi@joshstewart.me>
2019-04-18 20:29:46 -0400

    Try method from docs

--- src/skills/import-bukkits-data/listener.js
+++ src/skills/import-bukkits-data/listener.js
@@ -1,40 +1,42 @@
-const fetch = require("node-fetch");
+var request = require("request");
 
 module.exports = controller => {
-  controller.on("file_share", async (bot, message) => {
+  controller.on("file_share", (bot, message) => {
     if (
       message.files &&
       message.files[0] &&
       message.files[0].name === "bukkits-data.json"
     ) {
-      try {
-        const url = message.files[0].url_private;
+      // try {
+      const url = message.files[0].url_private;
 
-        const response = await fetch(url, {
-          headers: {
-            Authorization: "Bearer " + bot.config.bot.token // Authorization header with bot's access token
-          }
-        });
+      const opts = {
+        method: "GET",
+        url: url,
+        headers: {
+          Authorization: "Bearer " + bot.config.bot.token // Authorization header with bot's access token
+        }
+      };
 
+      request(opts, function(err, res, body) {
+        // body contains the content
+        console.log("FILE RETRIEVE STATUS", res.statusCode);
         console.log("**************");
-        console.log(JSON.stringify(response));
+        console.log(JSON.stringify(body));
         console.log("**************");
+      });
 
-        const json = await response.json();
-        console.log("**************");
-        console.log(JSON.stringify(json));
-        console.log("**************");
+      // console.log("**************");
+      // console.log(JSON.stringify(response));
+      // console.log("**************");
 
-        // const response = await fetch(url);
-        // bot.replyPublicDelayed(message, "GOT RESPONSE");
-        // const json = await response.json();
-        // console.log("*********");
-        // console.log(JSON.stringify(json));
-        // console.log("*********");
-        // bot.replyPublicDelayed(message, `${response}`);
-      } catch (err) {
-        bot.replyPublicDelayed(message, `${err}`);
-      }
+      // const json = await response.json();
+      // console.log("**************");
+      // console.log(JSON.stringify(json));
+      // console.log("**************");
+      // } catch (err) {
+      // bot.replyPublicDelayed(message, `${err}`);
+      // }
     }
   });
 };
