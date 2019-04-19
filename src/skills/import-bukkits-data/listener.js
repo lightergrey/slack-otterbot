27774ecce039d4886e26c7ddd6c99904a9d096a1
Josh Stewart <hi@joshstewart.me>
2019-04-18 20:48:37 -0400

    cleanup

--- src/skills/import-bukkits-data/listener.js
+++ src/skills/import-bukkits-data/listener.js
@@ -7,36 +7,23 @@ module.exports = controller => {
       message.files[0] &&
       message.files[0].name === "bukkits-data.json"
     ) {
-      // try {
       const url = message.files[0].url_private;
 
       const opts = {
         method: "GET",
         url: url,
         headers: {
-          Authorization: "Bearer " + bot.config.bot.token // Authorization header with bot's access token
+          Authorization: `Bearer ${bot.config.bot.token}`
         }
       };
 
       request(opts, function(err, res, body) {
-        // body contains the content
-        console.log("FILE RETRIEVE STATUS", res.statusCode);
+        const data = JSON.parse(body);
+        bot.reply(message, `FILE RETRIEVE STATUS: ${res.statusCode}`);
         console.log("**************");
-        console.log(JSON.stringify(body));
+        console.log(data);
         console.log("**************");
       });
-
-      // console.log("**************");
-      // console.log(JSON.stringify(response));
-      // console.log("**************");
-
-      // const json = await response.json();
-      // console.log("**************");
-      // console.log(JSON.stringify(json));
-      // console.log("**************");
-      // } catch (err) {
-      // bot.replyPublicDelayed(message, `${err}`);
-      // }
     }
   });
 };
